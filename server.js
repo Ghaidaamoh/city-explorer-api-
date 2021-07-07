6455d4bc2ebefbd7227fd423c82eeee124df3ffa
Ghaidaamoh <ghaidaa97mohammad@gmail.com>
2021-07-07 15:58:52 +0300

    backendlab08

--- server.js
+++ server.js
@@ -34,6 +34,32 @@ function getweatherHandler(req, res) {
             res.status(500).send(error)
         })
 }
+// localhost:3005/movies?cityName=Amman
+server.get("/movies",getmovieHandler); 
+  function getmovieHandler(req, res) {
+    let movieQuery = req.query.cityName;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${movieQuery}`;
+    axios
+      .get(url)
+      .then((moviesData) => {
+        res.status(200).send(
+          moviesData.data.results.map((movie) => {
+            return new Movies(
+              movie.title,
+              movie.overview,
+              movie.release_date,
+              movie.vote_average,
+              movie.vote_count,
+              movie.popularity,
+              movie.poster_path
+            );
+          })
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
 // Error
 server.get('*', (request, response) => {
@@ -50,3 +76,23 @@ server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 
 })
+
+class Movies {
+    constructor(
+      title,
+      overview,
+      released_on,
+      average_votes,
+      total_votes,
+      popularity,
+      poster_path
+    ) {
+      this.title = title;
+      this.overview = overview;
+      this.released_on = released_on;
+      this.average_votes = average_votes;
+      this.total_votes = total_votes;
+      this.image_url = "https://image.tmdb.org/t/p/w500/" + poster_path;
+      this.popularity = popularity;
+    }
+  }
